b8ef24e10b03f74c8560ec44d4effad661637507
TheiLLeniumStudios <104288623+TheiLLeniumStudios@users.noreply.github.com>
2022-05-10 13:34:12 +0000

    Fix linting issues##

--- server/main.lua
+++ server/main.lua
@@ -19,15 +19,14 @@ RegisterNetEvent("qb-pawnshop:server:sellPawnItems", function(itemName, itemAmou
     end
 end)
 
-RegisterNetEvent("qb-pawnshop:server:meltItemRemove", function(itemName, itemAmount,item)
+RegisterNetEvent("qb-pawnshop:server:meltItemRemove", function(itemName, itemAmount, item)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    local meltTime = 0
 
     if Player.Functions.RemoveItem(itemName, itemAmount) then
         TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[itemName], 'remove')
         
-        meltTime = (tonumber(itemAmount) * item.time)
+        local meltTime = (tonumber(itemAmount) * item.time)
         TriggerClientEvent('qb-pawnshop:client:startMelting', src, item, tonumber(itemAmount), (meltTime* 60000/1000))
 
         TriggerClientEvent("QBCore:Notify", src, Lang:t('info.melt_wait', {value = meltTime}), "primary")
@@ -39,13 +38,11 @@ end)
 RegisterNetEvent("qb-pawnshop:server:pickupMelted", function(item)
     local src = source
     local Player = QBCore.Functions.GetPlayer(src)
-    local meltedAmount = 0
-    local rewardAmount = 0
 
-    for k,v in pairs(item.items) do
-        meltedAmount = v.amount
-        for l,m in pairs(v.item.reward) do
-            rewardAmount = m.amount
+    for _, v in pairs(item.items) do
+        local meltedAmount = v.amount
+        for _, m in pairs(v.item.reward) do
+            local rewardAmount = m.amount
             Player.Functions.AddItem(m.item, (meltedAmount * rewardAmount))
             TriggerClientEvent('inventory:client:ItemBox', src, QBCore.Shared.Items[m.item], 'add')
             TriggerClientEvent('QBCore:Notify', src, Lang:t('success.items_received', {value = (meltedAmount * rewardAmount), value2 = QBCore.Shared.Items[m.item].label}), 'success')

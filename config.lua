388cc4b521e6c0a9afd29016c514e0588bcc6c59
Atikur Rahaman Santo <55808925+ARSSANTO@users.noreply.github.com>
2022-06-15 21:18:45 +0600

    Fresh Start

--- config.lua
+++ config.lua
@@ -1,6 +1,16 @@
 Config = {}
 
-Config.PawnLocation = vector3(412.34, 314.81, 103.13)
+Config.PawnLocation = {
+    coords = vector3(1137.57, -470.8, 66.66),
+    length = 1.5,
+    width = 1.8,
+    heading = 1.81,
+    debugPoly = false,
+    minZ = 65.66,
+    maxZ = 67.66,
+    distance = 2.0
+}
+
 Config.BankMoney = false -- Set to true if you want the money to go into the players bank
 Config.UseTimes = false -- Set to false if you want the pawnshop open 24/7
 Config.TimeOpen = 7 -- Opening Time
@@ -11,91 +21,91 @@ Config.UseTarget = GetConvar('UseTarget', 'false') == 'true'
 
 Config.PawnItems = {
     [1] = {
-        item = "goldchain",
+        item = 'goldchain',
         price = math.random(50,100)
     },
     [2] = {
-        item = "diamond_ring",
+        item = 'diamond_ring',
         price = math.random(50,100)
     },
     [3] = {
-        item = "rolex",
+        item = 'rolex',
         price = math.random(50,100)
     },
     [4] = {
-        item = "10kgoldchain",
+        item = '10kgoldchain',
         price = math.random(50,100)
     },
     [5] = {
-        item = "tablet",
+        item = 'tablet',
         price = math.random(50,100)
     },
     [6] = {
-        item = "iphone",
+        item = 'iphone',
         price = math.random(50,100)
     },
     [7] = {
-        item = "samsungphone",
+        item = 'samsungphone',
         price = math.random(50,100)
     },
     [8] = {
-        item = "laptop",
+        item = 'laptop',
         price = math.random(50,100)
     }
 }
 
 Config.MeltingItems = { -- meltTime is amount of time in minutes per item
     [1] = {
-        item = "goldchain",
+        item = 'goldchain',
         rewards = {
             [1] = {
-                item = "goldbar",
+                item = 'goldbar',
                 amount = 2
             }
         },
         meltTime = 0.15
     },
     [2] = {
-        item = "diamond_ring",
+        item = 'diamond_ring',
         rewards = {
             [1] = {
-                item = "diamond",
+                item = 'diamond',
                 amount = 1
             },
             [2] = {
-                item = "goldbar",
+                item = 'goldbar',
                 amount = 1
             }
         },
         meltTime = 0.15
     },
     [3] = {
-        item = "rolex",
+        item = 'rolex',
         rewards = {
             [1] = {
-                item = "diamond",
+                item = 'diamond',
                 amount = 1
             },
             [2] = {
-                item = "goldbar",
+                item = 'goldbar',
                 amount = 1
             },
             [3] = {
-                item = "electronickit",
+                item = 'electronickit',
                 amount = 1
             }
         },
         meltTime = 0.15
     },
     [4] = {
-        item = "10kgoldchain",
+        item = '10kgoldchain',
         rewards = {
             [1] = {
-                item = "diamond",
+                item = 'diamond',
                 amount = 5
             },
             [2] = {
-                item = "goldbar",
+                item = 'goldbar',
                 amount = 1
             }
         },

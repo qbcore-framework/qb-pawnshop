32e0c78be23d5816d550cadf795500a32c04d2e6
buddizer <none>
2022-09-21 22:28:09 -0600

    auto switch locale system

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,21 +5,21 @@ description 'QB-Pawnshop'
 version '1.0.0'
 
 shared_scripts {
-	'@qb-core/shared/locale.lua',
-	'config.lua',
-	'locales/en.lua',
+    '@qb-core/shared/locale.lua',
+    'config.lua',
+    'locales/en.lua',
     'locales/*.lua'
 }
 
 server_scripts {
-	'@oxmysql/lib/MySQL.lua',
-	'server/main.lua'
+    '@oxmysql/lib/MySQL.lua',
+    'server/main.lua'
 }
 
 client_scripts {
-	'@PolyZone/client.lua',
-	'@PolyZone/BoxZone.lua',
-	'@PolyZone/ComboZone.lua',
+    '@PolyZone/client.lua',
+    '@PolyZone/BoxZone.lua',
+    '@PolyZone/ComboZone.lua',
     'client/main.lua'
 }
 

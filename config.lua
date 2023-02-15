3ad4cd097a7d2622285d275965c073cae43aafd3
aalbonn <96874392+aalbonn@users.noreply.github.com>
2023-02-16 01:50:46 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -13,6 +13,7 @@ Config.PawnLocation = {
         },
     }
 
+Config.UseBlip = true -- Set to true if you want to use blips
 Config.BankMoney = false -- Set to true if you want the money to go into the players bank
 Config.UseTimes = false -- Set to false if you want the pawnshop open 24/7
 Config.TimeOpen = 7 -- Opening Time
